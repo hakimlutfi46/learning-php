ec209bb990be3fbfd04602ffef224ac375550754
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-08-31 06:04:26 +0700

    add new file operator

---
+++ operator2.php
@@ -0,0 +1,13 @@
+<?php 
+$x = 10; 
+$y = 6;
+echo ($x + $y); 
+echo ($x - $y); 
+echo ($x * $y); 
+echo ($x / $y); 
+echo ($x % $y); 
+$a = "Hello";
+$b = $a . " world!";
+echo $b; 
+echo $a . " -- " . $b . " ini string operator "; // Perhatikan penulisan tanda kutip
+?>
