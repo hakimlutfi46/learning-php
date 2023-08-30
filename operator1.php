3657818bbecb168fc0a2de26567e0d28fe577e62
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-08-31 06:00:14 +0700

    commit again

--- operator1.php
+++ operator1.php
@@ -2,7 +2,7 @@
 $tugas1=90;
 $tugas2=80;
 $jumlah=$tugas1 + $tugas2;
-$rerata=$jumlah/2 ;
+$rerata=$jumlah/2;
 echo "Nilai Tugas I :".$tugas1."<br>";
 echo "Nilai Tugas II:".$tugas2."<br>";
 echo "Jumlah Tugas :".$jumlah."<br>";
