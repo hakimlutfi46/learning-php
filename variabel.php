b94c81e8fd96c8bf4d30c8ff1d55a77f347e5327
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-09-01 13:20:48 +0700

    change file

--- variabel.php
+++ variabel.php
@@ -2,7 +2,7 @@
 $nim = "E12345678";
 $nama = "Budi";
 $prodi = "Teknik Informatika";
-echo "Selamat Praktikum";
-echo $nim;
-echo $nama;
-echo $prodi;
+echo "Selamat Praktikum \n";
+echo $nim . "\n";
+echo $nama . "\n";
+echo $prodi . "\n";
